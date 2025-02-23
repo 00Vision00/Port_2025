c3b9dfa84b94a847abcd68dea9d79758944da726
mugen-sample-code <mugen0414d@gmail.com>
2025-02-24 00:36:28 +0900

    new_0

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 
 import Base from "@/component/Base";
-import Button_index from "@/component/Button_index";
 import Head from "@/component/Head";
-import Menu from "@/component/Menu";
 import Projects from "@/component/Projects";
-import Scene from "@/component/Scene";
 import { useState } from 'react'
 
 export default function page() {
